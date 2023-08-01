69a163585494b34baa0210bec8b0ae7d78052ca6
Aiden <aiden0xz@gmail.com>
2023-08-01 14:58:59 +0800

    chore: gradle wrapper and docker compose file

---
+++ docker/prepare.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Preparing folder init and creating ./init/initdb.sql"
+mkdir ./init >/dev/null 2>&1
+chmod -R +x ./init
+docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > ./init/initdb.sql
+echo "done"
